f22c1a3a7ef065a5c288818323355c3c020679cc
ilaksono <60628041+ilaksono@users.noreply.github.com>
2020-11-15 20:17:01 -0500

    add seeds

--- src/hooks/useMapData.js
+++ src/hooks/useMapData.js
@@ -1,23 +1,12 @@
 import { useReducer } from 'react';
-
-const ADD_RESULTS = 'ADD_RESULTS';
-const reducer = (mapState, action) => {
-  switch(action.type) {
-
-    case ADD_RESULTS: {
-      return {...mapState, places: action.results}
-    }
-    default:
-      throw new Error('Invalid use map action type') 
-  }
-};
+import placeReducer, {ADD_RESULTS} from 'reducers/placeReducer.js';
 
 const initMapState = {
   places: []
 }
 
 const useMapData = () => {
-  const [mapState, dispatch] = useReducer(reducer, initMapState);
+  const [mapState, dispatch] = useReducer(placeReducer, initMapState);
 
   const addResults = (results) => {
     dispatch({type: ADD_RESULTS, results})
