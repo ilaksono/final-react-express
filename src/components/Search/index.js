778791101c9e7b9caea4e047adbe57f65b70cb1e
ilaksono <ian.laksono@gmail.com>
2020-11-18 23:29:44 -0500

    fix search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -19,35 +19,23 @@ const Search = props => {
 
   // function reset() {
   //   // resets the text data
+
   // }
   useEffect(() => {
     props.setRefinedSeed(props.results);
+    // eslint-disable-next-line
   }, [props.results]);
 
   const handleSearch = () => {
     props.yelpSearch(venue, location)
   };
-
   return (
-    <div className='search-container'>
-      <div className="venue-container">
-        <input type='text' value={venue} onChange={event => setVenue(event.target.value)} className="venue-container" />
-      </div>
-      <div className="location-container">
-        <input type='text' value={location} onChange={event => setLocation(event.target.value)} className="location-container" />
-      </div>
+    <div className="search-container">
+      <Venue venue={venue} onChange={setVenue} />
+      <Location location={location} onChange={setLocation} />
       <Button onClick={handleSearch} message={props.buttonMessage} search />
     </div>
   );
-
-
-  // return (
-  //   <div className="search-container">
-  //     <Venue venue={venue} onChange={setVenue} />
-  //     <Location location={location} onChange={setLocation} />
-  //     <Button onClick={handleSearch} message={props.buttonMessage} search />
-  //   </div>
-  // );
 };
 
 export default Search;
