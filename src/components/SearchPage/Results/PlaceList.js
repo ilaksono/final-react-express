749c1e01af3564328a575c65a55e349d795f0584
Andrew Markham <andrewjmarkham1@gmail.com>
2020-11-21 13:28:28 -0500

    add Sort to search result page

--- src/components/SearchPage/Results/PlaceList.js
+++ src/components/SearchPage/Results/PlaceList.js
@@ -1,6 +1,26 @@
 import PlaceListItem from "./PlaceListItem";
+import "styles/PlaceListItem.scss";
+import Sort from 'components/Sort';
 import { YelpContext } from 'YelpContext';
 import { useContext } from "react";
+
+const sortOptions = [
+  {
+    id: "comfort_rating",
+    value: "Comfort Rating",
+    default: true
+  },
+  {
+    id: "number_of_reviews",
+    value: "Number Of Reviews",
+    default: false
+  },
+  {
+    id: "yelp_rating",
+    value: "Yelp Rating",
+    default: false
+  }
+];
 
 const PlaceList = (props) => {
 
@@ -17,9 +37,11 @@ const PlaceList = (props) => {
   });
 
   return (
-    <div className=''>
-      <h2>Search Results</h2>
-      <span>Sort By:</span>
+    <div>
+      <div className="search-title-container">
+        <h2>Search Results</h2>
+        <Sort sortOptions={sortOptions} defaultOption={sortOptions[0].id} />
+      </div>
       {placeList}
     </div>
   );
