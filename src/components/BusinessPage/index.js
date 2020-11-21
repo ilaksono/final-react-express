47951cc491498aa55134136ea5beb5f0638479c0
Daniel <danschae@gmail.com>
2020-11-20 20:15:46 -0500

    add date to business page

--- src/components/BusinessPage/index.js
+++ src/components/BusinessPage/index.js
@@ -21,6 +21,7 @@ export default function BusinessPage() {
       return (
       <div class='business-container'>
       <div class='images'> 
+
           {businessDetails.photos && <img src={businessDetails.photos[0]} alt='photos' class='place-imgs-1'/>}
           {businessDetails.photos && <img src={businessDetails.photos[1]} lass='place-imgs-2'/>}
           {businessDetails.photos && <img src={businessDetails.photos[2]} lass='place-imgs-3'/>}
@@ -39,7 +40,15 @@ export default function BusinessPage() {
       </div>
       <div class='location-hours'>
           <span>Map</span><br/>
-          <span>Hours</span>
+          <table>
+      {businessDetails.hours && <tr>{`Monday:  ${businessDetails.hours[0].open[0].start} -  ${businessDetails.hours[0].open[0].end}`}  </tr> }
+      {businessDetails.hours && <tr>{`Tuesday:  ${businessDetails.hours[0].open[1].start} -  ${businessDetails.hours[0].open[0].end}`}  </tr> }
+      {businessDetails.hours && <tr>{`Wednesday:  ${businessDetails.hours[0].open[2].start} -  ${businessDetails.hours[0].open[0].end}`}  </tr> }
+      {businessDetails.hours && <tr>{`Thursday:  ${businessDetails.hours[0].open[3].start} -  ${businessDetails.hours[0].open[0].end}`}  </tr> }
+      {businessDetails.hours && <tr>{`Friday:  ${businessDetails.hours[0].open[4].start} -  ${businessDetails.hours[0].open[0].end}`}  </tr> }
+      {businessDetails.hours && <tr>{`Saturday:  ${businessDetails.hours[0].open[5].start} -  ${businessDetails.hours[0].open[0].end}`}  </tr> }
+      {businessDetails.hours && <tr>{`Sunday:  ${businessDetails.hours[0].open[6].start} -  ${businessDetails.hours[0].open[0].end}`}  </tr> }
+          </table>
         </div>
         <div class='reviews'>
         reviews
