dacc1ffe5337997e2b089ba1c8e32ee3517ea473
Andrew Markham <andrewjmarkham1@gmail.com>
2020-11-23 10:47:41 -0500

    search page styled

--- src/components/SearchPage/Results/PlaceListItem.js
+++ src/components/SearchPage/Results/PlaceListItem.js
@@ -43,53 +43,55 @@ const PlaceListItem = (props) => {
           props.hoverMarker({id:props.id, lat: props.latitude, lng: props.longitude})}
         onMouseOut={() =>
           props.notHoverMarker()}>
-        <div class='img-container'>
-          <img src={props.image} alt="Logo" class='venue-image' />
+        <div class="img-general-info-container">
+          <div class='img-container'>
+            <img src={props.image} alt="Logo" class='venue-image' />
+          </div>
+          <div class='general-info'>
+          <h3 onClick={moveToNextPage} class="venue_name">{props.label}. {props.name}</h3>
+          <div className="rating-outer-container">
+            <div className="rating-title">
+              Yelp Rating:
+            </div>
+            <Box component="fieldset" mb={0} pb={0} pt={0} borderColor="transparent">
+              <Rating name="read-only" precision={0.5} value={props.yelpRating} readOnly size="small" />
+            </Box>
+            <div className="covid_review_count">
+              ({props.yelpRatingCount})
+            </div>
+          </div>
+          <div className="rating-outer-container">
+            <div className="rating-title">
+              Safe Score:
+            </div>
+            <Box component="fieldset" mb={0} pb={0} pt={0} borderColor="transparent">
+              { isNaN(props.overall_rating) ? "N/A" 
+              : <StyledRating
+                  name="customized-color"
+                  size="small"
+                  value={props.overall_rating}
+                  precision={0.5}
+                  icon={<FavoriteIcon fontSize="inherit" />}
+                  readOnly
+                />}
+            </Box>
+            <div className="covid_review_count">
+              ({props.reviews.length})
+            </div>
+          </div>
+          <div className="sample-review-container">
+            {props.reviews.length > 0 && `"${props.reviews[0].description}"`}
+            <div className="sample-review-user">
+              {props.reviews.length > 0 && `-${props.reviews[0].user_id}`}
+            </div>
+          </div>
         </div>
-        <div class='general-info'>
-      <h3 onClick={moveToNextPage} class="venue_name">{props.label}. {props.name}</h3><br />
-      <div className="rating-outer-container">
-        <div className="rating-title">
-          Yelp Rating:
-        </div>
-        <Box component="fieldset" mb={0} pb={0} pt={0} borderColor="transparent">
-          <Rating name="read-only" precision={0.5} value={props.yelpRating} readOnly size="small" />
-        </Box>
-        <div className="covid_review_count">
-          ({props.yelpRatingCount})
-        </div>
-      </div>
-
-      <div className="rating-outer-container">
-        <div className="rating-title">
-          Safe Score:
-        </div>
-        <Box component="fieldset" mb={0} pb={0} pt={0} borderColor="transparent">
-          { isNaN(props.overall_rating) ? "N/A" 
-          : <StyledRating
-              name="customized-color"
-              size="small"
-              value={props.overall_rating}
-              precision={0.5}
-              icon={<FavoriteIcon fontSize="inherit" />}
-              readOnly
-            />}
-        </Box>
-        <div className="covid_review_count">
-          ({props.reviews.length})
-        </div>
-      </div>
-      <span class='yelp_rating'>Yelp Rating: {props.yelpRating}</span><br />
-      <span class='covid_rating'>Safe Score: {isNaN(props.overall_rating) ? "N/A" : props.overall_rating }</span>
-      <span class="covid_review_count">{props.reviews.length}</span><br />
-      <span class= "review-description">{props.reviews.length > 0 ? `Review: ${props.reviews[0].description}` : ""}</span>
+        
       </div>
       <div class='location'> 
-      <span>
         {props.phone}<br />
         {props.address}<br />
         {props.city}
-      </span>
       </div>
     </div>}
     </div>
