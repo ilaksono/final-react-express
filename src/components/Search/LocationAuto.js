331711e0b91426e98c124b9299d05419d6bf5c73
ilaksono <ian.laksono@gmail.com>
2020-11-30 17:59:46 -0500

    change location placeholder

--- src/components/Search/LocationAuto.js
+++ src/components/Search/LocationAuto.js
@@ -34,7 +34,7 @@ const LocationAuto = props => {
           }}
           className={`${props.isHome ? 'home-search-l' : 'location-search-bar'}`}
           disabled={false}
-          placeholder="Toronto"
+          placeholder="Location"
         />
         <ComboboxPopover style={{zIndex: 10}}>
           
