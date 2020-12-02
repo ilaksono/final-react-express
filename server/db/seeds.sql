6229d4d685624a4781dad9c4171da26e3747768d
Daniel <danschae@gmail.com>
2020-12-02 12:34:08 -0500

    add seeds

--- server/db/seeds.sql
+++ server/db/seeds.sql
@@ -1,13 +1,30 @@
--- INSERT INTO users(username, email, password, city, profile_pic)
--- VALUES ('DannyBoy', 'test1@test.ca', 'password', 'Montreal', 'https://vengreso.com/wp-content/uploads/2016/03/LinkedIn-Profile-Professional-Picture-Sample-Bernie-Borges.png'),
--- ('IanBein', 'test2@test.ca', 'password', 'Toronto', 'https://www.fairtravel4u.org/wp-content/uploads/2018/06/sample-profile-pic.png'),
--- ('Androo', 'test3@test.ca', 'password', 'Toronto', 'https://cdn-images.zety.com/authors/christian_eilers_1.jpg'),
--- ('Dog The Bounty Hunter', 'test4@test.ca', 'password', 'Vancouver', 'https://www.headshotsprague.com/wp-content/uploads/2019/07/Headshots_Prague-emotional-portrait-of-a-smiling-entrepreneur-1.jpg'),
--- ('Maurice Richarde', 'test5@test.ca', 'password', 'New York', 'https://dspncdn.com/a1/media/692x/7f/6a/2e/7f6a2e37bc1365bf37403821ffc8d958.jpg'),
--- ('Beyonce', 'test6@test.ca', 'password', 'Hollywood', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkWm-hMmPyzdDP6M8LzB1RlaGJkZ1om8zRkg&usqp=CAU'),
--- ('Lil Pimp', 'test7@test.ca','password', 'Detroit', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ93ak3v27x77a6YRkWqLWlAnvhm06VwngXEQ&usqp=CAU'),
--- ('The Brade Bunch', 'test8@test.ca', 'password', 'Log Angeles', 'https://swca.s3.amazonaws.com/4/g/54d177632ecc1f9f788389f3.2.w314.jpg');
+INSERT INTO users(username, email, password, city)
+VALUES ('DannyBoy', 'test7@test.ca', 'password', 'Montreal'),
+('try guy', 'test8@test.ca', 'password', 'Montreal'),
+('Mr.Magoo', 'test9@test.ca', 'password', 'Montreal'),
+('Mrs. Robinson', 'test10@test.ca', 'password', 'Montreal'),
+('Eddie', 'test11@test.ca', 'password', 'Montreal'),
+('Freddie', 'test12@test.ca', 'password', 'Montreal'),
+('Benny', 'test13@test.ca', 'password', 'Montreal');
 
--- INSERT INTO reviews (user_id, venue_id, venue_name, description, overall_rating, 
--- cleanliness, socialDistancing, transactionProcess)
+INSERT INTO reviews (user_id, venue_id, venue_name, description, overall_rating, 
+cleanliness, socialDistancing, transactionProcess, date)
+VALUES (7, 'Uotfj7HlJoNRp1pVv3AnDw', 'Starbucks', 'acceptable', 3, 3, 3, 3, '2020-11-30 00:02:03'),
+(8, 'Uotfj7HlJoNRp1pVv3AnDw', 'Starbucks', 'I was hoping for better', 3, 3, 3, 3, '2020-11-30 00:02:03'),
+(9, 'Uotfj7HlJoNRp1pVv3AnDw', 'Starbucks', 'Not great', 2, 2.5, 4, 3.5, '2020-11-29 00:02:03'),
+(10, 'Uotfj7HlJoNRp1pVv3AnDw', 'Starbucks', 'Mehh....', 3, 2, 3, 2, '2020-11-28 00:02:03'),
+(10, 'Uotfj7HlJoNRp1pVv3AnDw', 'Starbucks', 'Mehh....', 3, 2, 3, 2, '2020-11-28 00:02:03'),
+(1, 'oZ-KO8nCnSeSwGvN8OVgsg', 'Rockaberry', 'Not amazing', 3, 2, 3, 2, '2020-11-28 00:02:03'),
+(3, 'oZ-KO8nCnSeSwGvN8OVgsg', 'Rockaberry', 'Average...', 3, 3, 3, 3, '2020-11-30 00:02:03'),
+(6, 'oZ-KO8nCnSeSwGvN8OVgsg', 'Rockaberry', 'I thought they did a decent job', 3, 4, 3, 4, '2020-10-01 00:02:03'),
+(7, 'RsnANpovUnrwSohyo5x4_Q', 'Première Moisson', 'I thought they did a decent job', 3, 4, 3, 3, '2020-10-01 00:02:03'),
+(2, 'RsnANpovUnrwSohyo5x4_Q', 'Première Moisson', 'I thought they did a decent job', 3, 4, 3, 3, '2020-11-01 00:02:03'),
+(3, 'RsnANpovUnrwSohyo5x4_Q', 'Première Moisson', 'I thought they did a decent job', 3, 4, 3, 3, '2020-11-25 00:02:03'),
+(8, 'RsnANpovUnrwSohyo5x4_Q', 'Première Moisson', 'I thought they did a decent job', 3, 4, 3, 3, '2020-12-01 00:02:03'),
+(8, 'FQ9Q_WmEcAjHeyLzGq86mg', 'Cora', 'I thought they did a decent job', 3, 4, 4, 4, '2020-11-01 00:02:03'),
+(2, 'FQ9Q_WmEcAjHeyLzGq86mg', 'Cora', 'Pretty safe!', 3, 4, 4, 4, '2020-11-05 00:02:03'),
+(5, 'FQ9Q_WmEcAjHeyLzGq86mg', 'Cora', 'Acceptable to me', 3, 4, 4, 3.5, '2020-11-10 00:02:03'),
+(9, 'FQ9Q_WmEcAjHeyLzGq86mg', 'Cora', 'Thought it was pretty safe, it is not easy during a pandemic to keep things safe ', 4, 5, 4, 4.5, '2020-11-20 00:02:03'),
+(1, 'FQ9Q_WmEcAjHeyLzGq86mg', 'Cora', 'solid, when compared with other places i have gone to', 3, 4, 4, 3.5, '2020-11-25 00:02:03');
+
 
